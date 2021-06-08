a9d7a2cb91b5d7acfd45eff2f62557a1f834a262
华文杰 <1151668504@qq.com>
2021-06-08 08:17:17 +0800

    测试改动

--- .vscode/demo.js
+++ .vscode/demo.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    let box = document.getElementsByClassName('box')[0]
+    let box = document.getElementsByClassName('.box')[0]
     box.addEventListener('click', () => {
         alert('这是测试代码')
     })
